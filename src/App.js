419b942b701a338f0f2e4df884ed0fcbebcddd81
Idzel Jaimes <irjaimes@outlook.com>
2021-05-18 17:46:01 -0700

    lifted state to parent component, added hook for document title

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from "react";
-import About from "./components/About";
+import React, { useState } from "react";
 import Nav from "./components/Nav";
-import "./App.css";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
 
 function App() {
   // this is JSX. unlike HTML, you don't need to add it as a string because you're using webpack and React.
-  // add all components here to import them, should be named as the exported function
+  // this originally in the Nav component is now in the parent component ('lifting the state')
+  const [categories] = useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <div className="App">
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
-        <Nav></Nav>
-        <About></About>
+        <div>
+          <Gallery></Gallery>
+          <About></About>
+        </div>
       </main>
     </div>
   );
